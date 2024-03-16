8cbbfbd5b4d35f664e5a872289489915e4948262
LeviArchman <161912515+LeviArchman@users.noreply.github.com>
2024-03-16 21:18:45 +0700

    7 create object placement
    happy

--- gamerpg2d/src/main/GamePanel.java
+++ gamerpg2d/src/main/GamePanel.java
@@ -9,6 +9,7 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 import entity.Player;
+import object.SuperObject;
 import tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable {
@@ -36,7 +37,11 @@ public class GamePanel extends JPanel implements Runnable {
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
     public CollisionChecker cChecker = new CollisionChecker(this);
+    public AssetSetter aSetter = new AssetSetter(this);
     public Player player = new Player(this, keyH);
+    public SuperObject obj[] = new SuperObject[10];
+
+
 
     public GamePanel(){
 
@@ -45,6 +50,11 @@ public class GamePanel extends JPanel implements Runnable {
         this.setDoubleBuffered(true);
         this.addKeyListener(keyH);
         this.setFocusable(true);
+    }
+
+    public void setupGame(){
+
+        aSetter.setObject();
     }
 
     public void startGameThread(){
@@ -98,9 +108,17 @@ public class GamePanel extends JPanel implements Runnable {
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D)g;
-
+        // tile
         tileM.draw(g2);
 
+        // object
+        for(int i = 0; i < obj.length; i++){
+            if(obj[i] != null){
+                obj[i].draw(g2, this);
+            }
+        }
+
+        // player
         player.draw(g2);
 
         g2.dispose();
